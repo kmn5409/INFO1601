812d106e1fee5a16f554edd4db6a16ade5d96939
TriniMisery <36904042+TriniMisery@users.noreply.github.com>
2018-04-15 23:08:17 -0400

    Add files via upload

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,4 +1,3 @@
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -11,12 +10,55 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-function Hello(){
-    var val = localStorage.getItem("user_name");
-    if(val == null){
-        document.getElementById("Hello").innerHTML = "Hello Guest";
+function inputBar4Comments() {
+    var y = document.getElementsByClassName("logDiscus2");
+    y[0].style.visibility = "visible";
+    y[1].style.visibility = "visible";
+    y[2].style.visibility = "visible";
+ 
+}
+function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("id01");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        b = list.getElementsByTagName("LI");
+        //Loop through all list-items:
+        for (i = 0; i < (b.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*check if the next item should
+            switch place with the current item:*/
+            if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                /*if next item is alphabetically
+                lower than current item, mark as a switch
+                and break the loop:*/
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark the switch as done:*/
+            b[i].parentNode.insertBefore(b[i + 1], b[i]);
+            switching = true;
+        }
     }
-    else{
-        document.getElementById("Hello").innerHTML = "Hello " + val;
-    }
+}
+
+function addToList() {
+    document.getElementById("comments").submit();
+    var name = document.getElementsByName("fullname")[0].value;
+    var comment = document.getElementsByName("comment")[0].value;
+
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(comment);
+    node.appendChild(textnode);
+    document.getElementById("commentList").appendChild(node);
+  
 }
